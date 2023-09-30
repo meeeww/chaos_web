f75d24ad07c15ebcf5091e6e63fe7d84b2170a47
meeeww <thezasm03@gmail.com>
2023-09-30 05:03:44 +0200

    Fix routing & improved landing page

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -13,10 +13,10 @@ function Inicio() {
           <div className="w-[30%] hidden lg:flex">
             <img src={Sett} alt="Sett" className="lg:max-w-[25rem] lg:min-w-[25rem] lg:w-[25rem] lg:h-[13rem]"></img>
           </div>
-          <div className="w-[70%] mx-0 lg:mx-[5rem] ml-0 lg:ml-8 flex flex-col gap-4">
-            <p className="font-[500] text-2xl leading-7">Inscríbete en la Chaos Series</p>
-            <p className="font-[400] text-xl leading-7">Forma parte de la liga para unirte y conocer a nuevos jugadores, equipos y staff en un ambiente sano y perfecto para comenzar y mejorar tu carrera como jugador.</p>
-            <div className="flex justify-center">
+          <div className="w-[70%] mx-0 lg:mx-[5rem] ml-0 lg:ml-8 flex flex-col gap-6">
+            <p className="font-[500] text-3xl leading-7">Inscríbete en la Chaos Series</p>
+            <p className="font-[400] text-xl leading-7 w-auto lg:w-[50rem]">Forma parte de la liga para unirte y conocer a nuevos jugadores, equipos y staff en un ambiente sano y perfecto para comenzar y mejorar tu carrera como jugador.</p>
+            <div className="flex justify-start">
               <Button
                 className="px-4 text-black border-[var(--color-texto-highlight)] bg-transparent hover:!bg-[var(--color-texto-highlight)] hover:text-white"
                 radius="full"
